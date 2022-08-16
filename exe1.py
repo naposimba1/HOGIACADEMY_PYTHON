337a06dd22df4aee0dc9bc1beea2dcead00c7200
naposimba1 <naposimba.gmail.com>
2022-08-16 20:01:29 +0200

    Ajout exe

---
+++ exe1.py
@@ -0,0 +1,34 @@
+z= input("Andika marque yawe")
+   
+if (z=="Tecno") or (z=="Samsung"):
+	print("Tu peux entrer")
+else:
+	print("Retour")
+
+
+
+
+	
+
+""" C:\Users\ISTEEBU>cd "e:\APPLICATION\HOGI ACADEMY\PYTHON"
+
+C:\Users\ISTEEBU>e:
+
+e:\APPLICATION\HOGI ACADEMY\PYTHON>exe.py
+'exe.py' n’est pas reconnu en tant que commande interne        
+ou externe, un programme exécutable ou un fichier de commandes.
+
+e:\APPLICATION\HOGI ACADEMY\PYTHON>python exe1.py
+  File "e:\APPLICATION\HOGI ACADEMY\PYTHON\exe1.py", line 1
+    z= input("Andika marque yawe") 
+IndentationError: unexpected indent
+
+e:\APPLICATION\HOGI ACADEMY\PYTHON>python exe1.py
+Andika marque yaweTecno
+Tu peux entrer
+
+e:\APPLICATION\HOGI ACADEMY\PYTHON>Samsung
+'Samsung' n’est pas reconnu en tant que commande interne
+ou externe, un programme exécutable ou un fichier de commandes.
+
+e:\APPLICATION\HOGI ACADEMY\PYTHON> """
