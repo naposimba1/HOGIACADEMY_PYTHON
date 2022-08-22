aaf78537eef2f4509ade5b61c9baad71a8746d72
naposimba1 <naposimba.gmail.com>
2022-08-22 16:24:19 +0200

    tutjon

--- tutoJonathan/main.py
+++ tutoJonathan/main.py
@@ -1,3 +1,19 @@
-# les fonctions
+def register():
+    print("--- Register ---")
+    username = input("Username : ")
+    password = input("Mot de passe : ")
 
-print("Je ne suis pas de ton niveau")
+
+def menu_not_connected():
+    while True:
+        print("Bienvenue sur notre application ! (non connecté")
+        print("Choisissez une option")
+        print("1. Login")
+        print("2. S'inscrire")
+        choix = int(input())
+
+        if choix == 2:
+            register()
+
+
+menu_not_connected()
