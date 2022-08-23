cd172f4ea88bdafb1661714082a527f834a2c9e9
naposimba1 <naposimba.gmail.com>
2022-08-23 10:33:50 +0200

    sdd

--- tutoJonathan/main.py
+++ tutoJonathan/main.py
@@ -1,19 +1,25 @@
-def register():
-    print("--- Register ---")
-    username = input("Username : ")
-    password = input("Mot de passe : ")
+# def register():
+#     print("--- Register ---")
+#     username = input("Username : ")
+#     password = input("Mot de passe : ")
 
 
-def menu_not_connected():
-    while True:
-        print("Bienvenue sur notre application ! (non connecté")
-        print("Choisissez une option")
-        print("1. Login")
-        print("2. S'inscrire")
-        choix = int(input())
+# def menu_not_connected():
+#     while True:
+#         print("Bienvenue sur notre application ! (non connecté")
+#         print("Choisissez une option")
+#         print("1. Login")
+#         print("2. S'inscrire")
+#         choix = int(input())
 
-        if choix == 2:
-            register()
+#         if choix == 2:
+#             register()
 
 
-menu_not_connected()
+# menu_not_connected()
+
+def afficher_info_personne(nom):
+    # print("La personne est " toto + ", son age est", age, "ans")
+    print("Le nom comporte", len(nom), "caractères")
+
+    afficher_info_personne("Emile")
