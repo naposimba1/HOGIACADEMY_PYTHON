ee606e03290f8dd9fed6dc4cff5c1aba52e67b55
naposimba1 <naposimba.gmail.com>
2022-08-26 14:10:56 +0200

    sallereception

---
+++ sallereception/sallere/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+admin.site.register(Salle)
+
+# from .models import *
+
+# admin.site.register(Eleve)
+# admin.site.register(Cours)
+# admin.site.register(Point)
+# Register your models here.
---
+++ sallereception/sallere/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Salle(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nomClient = models.CharField(max_length=32)
+    prenomClient = models.CharField(max_length=32)
+    phoneClient = models.CharField(max_length=32)
+    # sexe = models.TextChoices("M", "F")
+    sexe = models.TextChoices("M", "F")
+    nomfete = models.CharField(max_length=32)
+    montantpaye = models.IntegerField()
+    datedelafete = models.DateField()
+    nombrejr = models.IntegerField()
+    autres = models.TextField()
+
+    def __str__(self):
+        return f"{self.nomclient} {self.prenomclient} {self.sexe} {self.nomfete} {self.datedelafete} {self.nombrejr} {self.autres}"
