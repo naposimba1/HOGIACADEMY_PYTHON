f6777f6cbe6b1e56fd9462432f1e471d28d99ed6
naposimba1 <naposimba.gmail.com>
2022-08-27 17:20:52 +0200

    ajout salle

--- sallereception/sallere/admin.py
+++ sallereception/sallere/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-from .models import Salle
-admin.site.register(Salle)
-
-# from .models import *
-
-# admin.site.register(Eleve)
-# admin.site.register(Cours)
-# admin.site.register(Point)
-# Register your models here.
+from .models import *
+admin.site.register(Client)
+admin.site.register(Type_Decor)
+admin.site.register(Type_Fete)
+admin.site.register(Reservation)
--- sallereception/sallere/models.py
+++ sallereception/sallere/models.py
@@ -1,27 +1,66 @@
+from ast import Delete
+from http import client
 from django.db import models
 
 # Create your models here.
 
 
-class Salle(models.Model):
+# class Type_Sexe(models.TextChoices):
+#     f = "feminin"
+#     m = "masculin"
+
+
+class Client(models.Model):
     id = models.BigAutoField(primary_key=True)
-    nomClient = models.CharField(max_length=32)
-    prenomClient = models.CharField(max_length=32)
-    phoneClient = models.CharField(max_length=32)
-    # sexe = models.TextChoices("M", "F")
-    #sexe = models.TextChoices("M", "F")
-    nomfete = models.CharField(max_length=32)
-    montantapaye = models.IntegerField()
-    avance = models.IntegerField()
-    datedelafete = models.DateField()
-    nombrejr = models.IntegerField()
-    autres = models.TextField()
+    nom_client = models.CharField(max_length=32)
+    prenom_client = models.CharField(max_length=32)
+    phone_client = models.CharField(max_length=32)
+    sexe = models.CharField(max_length=10)
+    cni = models.CharField(max_length=32)
 
     def __str__(self):
-        return f"{self.nomClient} {self.prenomClient} {self.nomfete} {self.datedelafete} {self.nombrejr} {self.autres}"
+        return f"{self.nom_client} {self.prenom_client} {self.phone_client} {self.sexe} {self.cni}"
 
 
-# class SalleAdmin(admin.ModelAdmin):
-#     list_display = ('nomClient', 'prenomClient', 'phoneClient',
-#                     'nomfete', 'montantpaye', 'datedelafete', 'nombrejr')
-#     list_filter = ('nomClient')
+class Type_Fete(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nom_fete = models.CharField(max_length=32)
+    montant_fete = models.IntegerField(default=0)
+    code_fete = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.nom_fete} {self.montant_fete} {self.code_fete}"
+
+
+class Type_Decor(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nomdecor = models.CharField(max_length=32)
+    montant_decor = models.IntegerField(default=0)
+    code_decor = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.nomdecor} {self.montant_decor} {self.code_decor}"
+
+
+class Reservation(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    type_decor = models.ForeignKey(
+        Type_Decor, on_delete=models.PROTECT, null=True, blank=True)
+    type_fete = models.ForeignKey(Type_Fete, on_delete=models.PROTECT)
+    a_payer = models.IntegerField(default=0, null=True, blank=True)
+    avance = models.IntegerField(default=0)
+    date_avance = models.DateTimeField(auto_now=True)
+    date_payer_total = models.DateField(null=True, blank=True)
+    date_reservation = models.DateField
+   # surcharge des methodes   :: guteranya le montant de la fête et le montant du décor
+
+    def save(self, *args, **kwargs):
+        if (self.type_decor):
+            self.a_payer = self.type_fete.montant_fete+self.type_decor.montant_decor
+        else:
+            self.a_payer = self.type_fete.montant_fete
+        super(Reservation, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.client.nom_client} {self.client.prenom_client} {self.date_avance} {self.date_reservation}"
